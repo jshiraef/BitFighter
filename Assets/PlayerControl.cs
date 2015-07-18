24f307263b93191e3dfd1d4225ba520bf251dae4
jshiraef <joeshiraef@gmail.com>
2015-07-17 23:21:19 -0400

    Character's moving. simple collision

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed;
+
+	public Direction direction;
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		this.direction = Direction.EAST;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Movement ();
+	}
+
+	void Movement()
+	{
+		if(Input.GetKey (KeyCode.D))
+		{
+			transform.Translate(Vector2.right * 4f * Time.deltaTime);
+			this.direction = Direction.EAST;
+			anim.Play ("WalkRight");
+		}
+		
+		else if(Input.GetKey (KeyCode.A))
+		{
+			transform.Translate (-Vector2.right * 4f * Time.deltaTime);
+			this.direction = Direction.WEST;
+			anim.Play ("WalkRight");
+			transform.eulerAngles = new Vector2(0, 180);
+		}
+
+		else 
+			{
+				if(this.direction == Direction.EAST)
+					{
+						anim.Play("idle");
+					}
+				else anim.Play ("idleLeft");
+			}	
+	
+	}
+	
+	public enum Direction
+	{
+		EAST, WEST
+	}
+	
+
+}
---
+++ Assets/bettyController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bettyController : MonoBehaviour {
+
+	public float speed;
+	
+	public Direction direction;
+	
+	public Animator anim;
+	
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		this.direction = Direction.EAST;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Movement ();
+	}
+	
+	void Movement()
+	{
+		if(Input.GetKey (KeyCode.J))
+		{
+			transform.Translate(Vector2.right * 4f * Time.deltaTime);
+			this.direction = Direction.EAST;
+			anim.Play ("BettyWalkRight");
+		}
+		
+		else if(Input.GetKey (KeyCode.L))
+		{
+			transform.eulerAngles = new Vector2(0, 180);
+			transform.Translate (-Vector2.right * 4f * Time.deltaTime);
+			this.direction = Direction.WEST;
+			anim.Play ("BettyWalkRight");
+		}
+		
+		else 
+		{
+			if(this.direction == Direction.EAST)
+			{
+				anim.Play("BettyIdleRight");
+			}
+			else anim.Play ("BettyIdleLeft");
+		}	
+		
+	}
+	
+	public enum Direction
+	{
+		EAST, WEST
+	}
+}
